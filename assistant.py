11898ae41308455e361203f5aa87db38e3891a79
Haroon Qureshi <Hrqureshi460@gmail.com>
2026-06-23 13:05:57 +1000

    Refactor Chatbot in functions

--- assistant.py
+++ assistant.py
@@ -3,19 +3,31 @@ from config import API_KEY
 
 client = genai.Client(api_key=API_KEY)
 
-print("Welcome to Chatbot! Type quit to exit.")
 
-conversation = []
-
-while True:
-    user_message = input("\nYou: ")
-    if user_message == "quit":
-        print("Goodbye!")
-        break
-    conversation.append({"role": "user", "parts": [{"text": user_message}]})
+def get_ai_response(conversation):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=conversation
     )
-    print("Gemini:", response.text)
-    conversation.append({"role": "model", "parts": [{"text": response.text}]})
+    return response.text
+
+
+def chat():
+    print("Welcome to Chatbot! Type quit to exit.")
+    conversation = []
+
+    while True:
+        user_message = input("\nYou: ")
+        if user_message == "quit":
+            print("Goodbye!")
+            break
+
+        conversation.append({"role": "user", "parts": [{"text": user_message}]})
+
+        reply = get_ai_response(conversation)
+        print("Gemini:", reply)
+
+        conversation.append({"role": "model", "parts": [{"text": reply}]})
+
+
+chat()
